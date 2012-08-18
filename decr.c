82236ce38690943aec12dc548c8b5aaedd9522ec
dlion <domenico.luciani@email.it>
2012-08-18 17:09:17 +0200

    Decripta

---
+++ decr.c
@@ -0,0 +1,52 @@
+#include <highgui.h>
+#include <cv.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    if(argc != 2)
+    {
+        printf("%s <image_in>\n",argv[0]);
+        return -1;
+    }
+
+    IplImage *in = cvLoadImage(argv[1]);
+    
+    if(in->nChannels != 3)
+    {
+        puts("Only RGB images");
+        return -1;
+    }
+
+    int i,j,k=0;
+    int height,width,step,channels;
+    uchar *data;
+    char lett;
+
+    height = in->height;
+    width = in->width;
+    step = in->widthStep;
+    channels = in->nChannels;
+    data = (uchar*)in->imageData;
+
+    for(i=0; i < height; i++)
+    {
+        for(j=0; j < width; j++)
+        {
+            lett = data[i*step+j*channels];
+            if(lett == '$')
+            {
+                i = height;
+                j = width;
+            }
+            else
+            {
+                printf("TROVATA: %c\n",lett);
+            }
+        }
+    }
+    cvReleaseImage(&in);
+
+    return 0;
+}
+
